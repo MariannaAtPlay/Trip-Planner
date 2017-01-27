baf9c1437c7cf9928bf0e3fdc0a83c78e7763ee1
Jada Hampton <jadachampton@gmail.com>
2017-01-27 17:17:26 -0500

    worked on models

--- models/index.js
+++ models/index.js
@@ -3,6 +3,30 @@ var db = new Sequelize('postgres://localhost:5432/tripPlanne', {
     logging: false
 });
 
+var Place = db.define('place', {
+    address: Sequelize.STRING,
+    city: Sequelize.STRING,
+    state: Sequelize.STRING,
+    phone: Sequelize.STRING,
+    location: Sequelize.ARRAY(Sequelize.FLOAT)
+}),
+
+var Hotel = db.define('hotel', {
+    name: Sequelize.STRING,
+    num_stars: {
+        type: Sequelize.INTEGER,
+        validate:{
+            min: 1,
+            max: 5
+        } 
+    },
+    amenities: Sequelize.STRING
+}),
+
+var Activity = db.define('activity', {
+    name: Sequelize.STRING,
+    age_range: 
+})
 module.exports = {
     db: db
 };
