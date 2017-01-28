802db953ca555db89e3594d591354929c9eff048
Marianna Shvartsapel <marianna.shvartsapel@gmail.com>
2017-01-27 19:33:09 -0500

    trying to seed the db

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var Sequelize = require('sequelize');
-var db = new Sequelize('postgres://localhost:5432/tripPlanne', {
+var db = new Sequelize('postgres://localhost:5432/tripPlanner', {
     logging: false
 });
 
@@ -9,7 +9,7 @@ var Place = db.define('place', {
     state: Sequelize.STRING,
     phone: Sequelize.STRING,
     location: Sequelize.ARRAY(Sequelize.FLOAT)
-}),
+});
 
 var Hotel = db.define('hotel', {
     name: Sequelize.STRING,
@@ -21,13 +21,37 @@ var Hotel = db.define('hotel', {
         } 
     },
     amenities: Sequelize.STRING
-}),
+});
 
 var Activity = db.define('activity', {
     name: Sequelize.STRING,
-    age_range: 
-})
+    age_range: Sequelize.STRING
+});
+
+var Restaurant = db.define('restaurant', {
+	name: Sequelize.STRING,
+	cuisine: Sequelize.STRING,
+	price: {
+        type: Sequelize.INTEGER,
+        validate:{
+            min: 1,
+            max: 5
+        } 
+    }
+});
+
+Hotel.belongsTo(Place);
+Activity.belongsTo(Place);
+Restaurant.belongsTo(Place);
+
+
+
+
 module.exports = {
+	place: Place,
+	hotel: Hotel,
+	activity: Activity,
+	restaurant: Restaurant,
     db: db
 };
 
